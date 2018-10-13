a1933b88fe23b5dd528a3298a4ca219b7109929c
Satyuki1 <tangcheng475899188@Gmail.com>
2018-10-13 21:32:10 +0800

    Create 输入一个数.java

---
+++ 输入一个数.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class lianxi {
+	public static void main(String[]args) {
+		int age;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("what is you age");
+		age=scan.nextInt();
+		System.out.println(age);
+			
+	}
+}
