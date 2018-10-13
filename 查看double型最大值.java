243ececc057cda90350afad0c71e6bc635f8e9d6
Satyuki1 <tangcheng475899188@Gmail.com>
2018-10-13 21:34:48 +0800

    Create 查看double型最大值.java

---
+++ 查看double型最大值.java
@@ -0,0 +1,10 @@
+public class lianxi {
+	public static void main(String[]args) {
+		double a=Double.MAX_VALUE;
+		float b=Float.MAX_VALUE;
+		System.out.println(a);
+		System.out.println(b);
+	}
+	
+
+}
